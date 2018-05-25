19910c6a8bcb693d89bb35dc0a7c7d1602fc80bb
Alexander Chaschin <chaschin@uchi.ru>
2018-05-25 14:20:35 +0300

    simplify test

--- test/getInformation.test.js
+++ test/getInformation.test.js
@@ -1,21 +1,12 @@
 import assert from 'assert';
 import Information from '../src/getInformation.js';
-describe('Information', () => {
-  describe('#process(message)', () => {
-    it('should trim', () => {
-      const message = '   привет      ';
-      const result = Information.process(message);
-      assert.equal(result, 'привет');
-    });
-    it('should lowercase', () => {
-      const message = 'прИВет';
-      const result = Information.process(message);
-      assert.equal(result, 'привет');
-    });
-    it('should replace non-russian symbols', () => {
-      const message = 'AFASsgdCCCприв###ет124';
-      const result = Information.process(message);
-      assert.equal(result, 'привет');
-    });
+describe('Information#process(message)', () => {
+  const tests = [
+    ['should trim', '   привет      ', 'привет'],
+    ['should lowercase', 'прИВет', 'привет'],
+    ['should replace non-russian symbols', 'AFASsgdCCCприв###ет124', 'привет']
+  ];
+  tests.forEach(([shouldWhat, message, expected]) => {
+    it(shouldWhat, () => assert.equal(Information.process(message), expected));
   });
 });
