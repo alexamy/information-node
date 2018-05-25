ac2ab44ead56b286da2ccb572bbb61e975c12a14
Alexander Chaschin <chaschin@uchi.ru>
2018-05-25 13:21:31 +0300

    update script, node version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Hello');
+import { readFile } from 'fs';
+
+function processFile(error, data) {
+  error ? console.log(error) : console.log(data);
+}
+
+const messagePath = process.argv[2];
+if (!messagePath) throw new Error('No path provided!');
+
+readFile(messagePath, 'utf-8', processFile);
