7075d1a9b5a810643196cdfeca8ed90bc352a9ee
alexij <achasch@gmail.com>
2018-05-27 23:49:01 +0300

    length cc

--- src/antijam.js
+++ src/antijam.js
@@ -2,5 +2,9 @@ export class AntiJamD3 {
   // codePhrase only from 0s and 1s
   constructor(codePhrase) {
     this.codePhrase = codePhrase;
+    this.numberIC = this.codePhrase.length;
+    this.numberCC = Math.ceil(
+      Math.log2(this.numberIC + 1 + Math.ceil(Math.log2(this.numberIC)))
+    );
   }
 }
--- test/antijam.test.js
+++ test/antijam.test.js
@@ -1,2 +1,10 @@
 import 'chai/register-should';
 import { AntiJamD3 } from '../src/antijam.js';
+
+const codePhrase = '0101';
+
+describe('AntiJamD3', () => {
+  it('should set number of control symbols correctly', () => {
+    new AntiJamD3(codePhrase).numberCC.should.equal(3);
+  });
+});
