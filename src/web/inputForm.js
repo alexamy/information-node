bc782f46264a53355e41ac2806c942639c831d87
Alexander Chaschin <chaschin@uchi.ru>
2018-05-28 17:44:17 +0300

    controlled component

--- src/web/inputForm.js
+++ src/web/inputForm.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 export class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    console.log('clack');
+  }
+
   render() {
     return (
       <form>
         <label>
-          Message:<br />
-          <input type="text" name="name" />
+          <div>Message:</div>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
         </label>
       </form>
     );
