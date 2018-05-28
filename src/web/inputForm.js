c87179ece0d9d167ab371873123c1e4b8687bb2e
Alexander Chaschin <chaschin@uchi.ru>
2018-05-28 17:36:49 +0300

    input

--- src/web/inputForm.js
+++ src/web/inputForm.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 export class InputForm extends Component {
   render() {
-    return <div>Input</div>;
+    return (
+      <form>
+        <label>
+          Message:<br />
+          <input type="text" name="name" />
+        </label>
+      </form>
+    );
   }
 }
