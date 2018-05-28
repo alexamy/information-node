0cf0a70b5801d9ea1b92b8cb6d4a4ebdad45f37f
Alexander Chaschin <chaschin@uchi.ru>
2018-05-28 16:09:17 +0300

    info test react

--- src/web/app.js
+++ src/web/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Information } from '../information.js';
+
+const info = new Information('privetikiki');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <p> {info.entropy} </p>
       </div>
     );
   }
