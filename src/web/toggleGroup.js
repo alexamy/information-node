b5d89d0bcd801c4d66423d631dbb2047f077b643
alexij <achasch@gmail.com>
2018-05-30 00:26:57 +0300

    toggleGroup

--- src/web/toggleGroup.js
+++ src/web/toggleGroup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { VelocityComponent } from 'velocity-react';
+import { VelocityComponent, velocityHelpers } from 'velocity-react';
+import 'velocity-animate/velocity.ui';
 import styled from 'styled-components';
 import $ from 'jquery';
 
@@ -8,13 +9,13 @@ const View = styled.div`
   cursor: pointer;
 `;
 const Header = styled.div`
-  border: 1px solid grey;
+  border: 1px solid #aaaaaa;
   padding: 0 10px;
 `;
 const Content = styled.div`
   overflow: hidden;
   position: relative;
-  border: 1px solid grey;
+  border: 1px solid #aaaaaa;
   border-top: 0;
   padding: 0 10px;
 `;
@@ -23,23 +24,35 @@ export class ToggleGroup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false
+      expanded: true
     };
+    this.speed = 0.2;
     this.contentRef = React.createRef();
     this.changeState = this.changeState.bind(this);
   }
 
   componentDidMount() {
+    const height = $(this.contentRef.current).height();
     this.setState({
       expanded: false,
-      height: this.contentRef.current.offsetHeight
+      height
+    });
+    this.hideAnim = velocityHelpers.registerEffect({
+      calls: [[{ height: 0, borderBottomColor: '#ffffff' }]]
     });
   }
 
   changeState(e) {
     e.preventDefault();
+    const height = this.contentRef.current.offsetHeight;
+    this.showAnim = velocityHelpers.registerEffect({
+      calls: [[{ height, borderBottomColor: '#aaaaaa' }]],
+      reset: { height: '100%' }
+    });
+
     this.setState(prevState => ({
-      expanded: !prevState.expanded
+      expanded: !prevState.expanded,
+      height
     }));
   }
 
@@ -48,7 +61,7 @@ export class ToggleGroup extends Component {
       <View onClick={this.changeState} onMouseDown={() => false}>
         <Header>{this.props.header}</Header>
         <VelocityComponent
-          animation={{ height: this.state.expanded ? this.state.height : 0 }}
+          animation={this.state.expanded ? this.showAnim : this.hideAnim}
           duration={500}
         >
           <Content>
