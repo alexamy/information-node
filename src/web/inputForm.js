ccd47e4be235da6e67a3998a59ba5afe4bf8af9a
Alexander Chaschin <chaschin@uchi.ru>
2018-05-29 09:39:18 +0300

    bootstrap

--- src/web/inputForm.js
+++ src/web/inputForm.js
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const Input = styled.input`
   width: 100%;
   font-size: 24px;
 `;
 export class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.handleChange(e.target.value);
+  }
+
   render() {
     return (
       <form>
@@ -14,7 +23,7 @@ export class InputForm extends Component {
           <Input
             type="text"
             value={this.props.value}
-            onChange={this.props.handleChange}
+            onChange={this.handleChange}
           />
         </label>
       </form>
